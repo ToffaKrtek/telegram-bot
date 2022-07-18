34ba33e1f86c9f7f14103b20c1fd4c57a646844e
ToffaKrtek <krotov.anthon@yandex.ru>
2022-07-18 13:43:58 +0700

    заготовка сервера и сервиса для работы с ним

--- app/app.js
+++ app/app.js
@@ -1,6 +1,10 @@
 import Bot from './bot.js'
 import token from './config.js'
+import Server from './server.js'
+const port = 3000;
 
 
 const bot = new Bot(token);
 bot.listeners()
+
+const server = new Server(port, bot)
