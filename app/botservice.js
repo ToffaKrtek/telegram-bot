9e346dff7f9f83e6ac061a146199d7d45bf5186f
ToffaKrtek <krotov.anthon@yandex.ru>
2022-07-22 18:00:05 +0700

    Возможность сохранить чат локально, прием-пересылка сообщений через прослушиваемый порт

--- app/botservice.js
+++ app/botservice.js
@@ -5,9 +5,17 @@ class BotService {
   }
 
   queryProcessing(query){
-    const data = JSON.parse(query);
+    return new Promise( (resolve, reject) => {
+//      const data = JSON.parse(query);
 
-    return null
+      if (query.author && query.msg){
+        this.bot.NewMessage(query.author, query.msg)
+      }else {
+        console.log(typeof(query), query)
+      }
+      resolve(true)
+    })
+
   }
 }
 
